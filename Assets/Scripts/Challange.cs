d5079a3f785368b6f424b26fed3d821ba081f67e
Spider <silentwalk15@gmail.com>
2016-09-10 14:25:22 +0200

    Added procedural generation. Currently only uses two seperate chunks.

---
+++ Assets/Scripts/Challange.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Challange : MonoBehaviour
+{
+    public GameObject[] challenges;
+    private Vector3 startPos;
+    private Vector3 endPos;
+
+    private void Awake()
+    {
+        this.startPos = this.transform.FindChild("StartPoint").transform.localPosition;
+        this.endPos = this.transform.FindChild("EndPoint").transform.localPosition;
+    }
+
+    public void SpawnNew()
+    {
+        Challange newChallenge = GenerateRandomChallenges();
+        newChallenge.transform.position = this.transform.position + this.endPos - newChallenge.startPos;
+    }
+
+    private Challange GenerateRandomChallenges()
+    {
+        GameObject newChallenge = Instantiate(challenges[Random.Range(0, challenges.Length)], transform.position, Quaternion.identity) as GameObject;
+        return newChallenge.GetComponent<Challange>();
+    }
+}
+
---
+++ Assets/Scripts/GenerateChallenge.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateChallenge : MonoBehaviour
+{
+
+    //public GameObject[] challenges;
+    //Transform challengesSpawnPoint;
+    //bool isGenerated = false;
+    //public Vector3 startPoint, endPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        //challengesSpawnPoint = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //void GenerateRandomChallenge()
+    //{
+    //    gameObject.SetActive(true);
+    //    //GameObject newChallenge = Instantiate(challenges[Random.Range(0, challenges.Length)], challengesSpawnPoint.position, Quaternion.identity) as GameObject;
+    //    GameObject newChallenge = Instantiate(challenges[Random.Range(0, challenges.Length)], transform.position, Quaternion.identity) as GameObject;
+    //    newChallenge.transform.parent = null;
+    //    isGenerated = true;
+    //}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            //GenerateRandomChallenge();
+            gameObject.SetActive(true);
+            GetComponentInParent<Challange>().SpawnNew();
+            gameObject.SetActive(false);
+            //if (isGenerated)
+            //{
+                
+
+            //}
+        }
+    }
+
+    //void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    Debug.Log(other.name);
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        if (!isGenerated && CompareTag("EndPointGround"))
+    //        {
+    //            GenerateRandomChallenge();
+    //            GetComponentInParent<Challange>().SpawnNew();
+    //        }
+    //    }
+    //}
+}
+
+
+
+//void GenerateRandomChallenge()
+//{
+//    GameObject newChallenge = Instantiate(challenges[Random.Range(0, challenges.Length)], transform.position, Quaternion.identity) as GameObject;
+//    newChallenge.transform.parent = null;
+//    isGenerated = true;
+//}
+
+//void OnTriggerEnter2D(Collider2D other)
+//{
+//    Debug.Log(other.name);
+//    if (other.CompareTag("Player"))
+//    {
+//        if (!isGenerated && CompareTag("EndPointGround"))
+//        {
+//            GenerateRandomChallenge();
+//        }
