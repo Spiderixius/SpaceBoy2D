5233108621a16a9ed565ffdf0b1481077d3b32a0
Spider <silentwalk15@gmail.com>
2016-09-19 13:03:43 +0200

    Added a mist monster that destroys everything in its path! DUN DUN DUUUN!

---
+++ Assets/Scripts/Procedural Generation/TheDestroyer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheDestroyer : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+        rb.isKinematic = false;
+
+        if (other.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.transform.position.y < -30)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+}
