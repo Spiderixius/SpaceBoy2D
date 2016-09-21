23433f81f0dd579ad2c9fe40a8c8d27a3fb1063b
Spider <silentwalk15@gmail.com>
2016-09-21 06:22:17 +0200

    Added death and respawning

---
+++ Assets/Scripts/Restarter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Restarter : MonoBehaviour {
+
+    public string levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+        //rb.isKinematic = false;
+
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
