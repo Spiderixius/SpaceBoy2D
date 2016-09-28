2738c11ce753316d59d5d102eabcdf33a97188d6
Spider <silentwalk15@gmail.com>
2016-09-28 12:56:59 +0200

    Improved the camera and added a spinning saw that is also able to move.

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -33,29 +33,33 @@ public class CameraController : MonoBehaviour {
 
     private void FollowTarget(GameObject target, float followAhead, float smoothing, Vector3 targetPosition)
     {
-        if (followTarget)
+        if (target != null)
         {
-            targetPosition = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
-
-            // This moves the target of the camera ahead of the player
-            if (target.transform.localScale.x > 0f)
+            if (followTarget)
             {
-                targetPosition = new Vector3(targetPosition.x + followAhead, targetPosition.y, targetPosition.z);
-            }
-            else
-            {
-                targetPosition = new Vector3(targetPosition.x - followAhead, targetPosition.y, targetPosition.z);
+                targetPosition = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
+
+                // This moves the target of the camera ahead of the player
+                if (target.transform.localScale.x > 0f)
+                {
+                    targetPosition = new Vector3(targetPosition.x + followAhead, targetPosition.y, targetPosition.z);
+                }
+                else
+                {
+                    targetPosition = new Vector3(targetPosition.x - followAhead, targetPosition.y, targetPosition.z);
+                }
+
+                // To avoid having different smoothing speeds for different frame rates, we will multiply with deltaTime.
+                transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime);
+
+                if (bounds)
+                {
+                    transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPosition.x, maxCameraPostion.x),
+                        Mathf.Clamp(transform.position.y, minCameraPosition.y, maxCameraPostion.y),
+                        Mathf.Clamp(transform.position.z, minCameraPosition.z, maxCameraPostion.z));
+                }
             }
 
-            // To avoid having different smoothing speeds for different frame rates, we will multiply with deltaTime.
-            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime);
-
-            if (bounds)
-            {
-                transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPosition.x, maxCameraPostion.x),
-                    Mathf.Clamp(transform.position.y, minCameraPosition.y, maxCameraPostion.y),
-                    Mathf.Clamp(transform.position.z, minCameraPosition.z, maxCameraPostion.z));
-            }
         }
     }
 }
